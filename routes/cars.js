f47940ef87f1ab90fd923e22beb8f9d1c12536d2
UzairAhmad-1 <61177616+UzairAhmad-1@users.noreply.github.com>
2021-05-27 19:52:32 +0500

    images done but not retrieve

--- routes/cars.js
+++ routes/cars.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
 
-const {promisify} = require("util");
+const { promisify } = require("util");
 const pipeline = promisify(require("stream").pipeline);
 
 const { Car } = require("../models/Car");
@@ -20,68 +20,91 @@ router.get("/:id", async (req, res) => {
   res.send(cars);
 });
 
-// var images = [];
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public");
+    console.log(file);
+    const vin = req.body.vin;
+    const type = file.fieldname;
+    if (!fs.existsSync("./public/" + vin)) {
+      fs.mkdirSync("./public/" + vin);
+      fs.mkdirSync("./public/" + vin + "/displayImage");
+      fs.mkdirSync("./public/" + vin + "/otherImages");
+      fs.mkdirSync("./public/" + vin + "/sendImages");
+    }
+    cb(null, "public/" + vin + "/" + type);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-var upload = multer({storage:storage});
+var upload = multer({ storage: storage });
 
-router.post("/", upload.array("file"), async (req, res) => {
-  var images = [];
-  console.log(req.body);
-  const {files} = req;
-  for (let x = 0; x < files.length; x++) {
-    images.push(files[x].originalname);
-  }
-  const monthlyPayment =
-    (parseInt(req.body.price) - parseInt(req.body.downPayment)) /
-    parseInt(req.body.numberOfMonths);
-  console.log(monthlyPayment);
-  let car = new Car({
-    make: req.body.make,
-    name: req.body.name,
-    model: req.body.model,
-    images: images,
-    fuel: req.body.fuel,
-    milesDriven: req.body.milesDriven,
-    body: req.body.body,
-    modelYear: req.body.modelYear,
-    exteriorColor: req.body.exteriorColor,
-    interiorColor: req.body.interiorColor,
-    engineType: req.body.engineType,
-    engineCapacity: req.body.engineCapacity,
-    transmission: req.body.transmission,
-    driveTrains: req.body.driveTrain,
-    doors: req.body.doors,
-    numberOfKeys: req.body.numberOfKeys,
-    vin: req.body.vin,
-    stock: req.body.stock,
-    vehicleId: req.body.vehicleId,
-    mpg: req.body.mpg,
-    extendedFeatures: req.body.extendedFeatures,
-    downPayment: req.body.downPayment,
-    monthlyPayment: monthlyPayment,
-    price: req.body.price,
-    shippingCharges: req.body.shippingCharges,
-    taxAndRegistrationCharges: req.body.taxAndRegistrationCharges,
-    dealerFees: req.body.dealerFees,
-  });
+router.post(
+  "/",
+  upload.fields([
+    { name: "displayImage" },
+    { name: "otherImages" },
+    { name: "sendImages" },
+  ]),
+  async (req, res) => {
+    var otherImages = [];
+    var sendImages = [];
+    console.log(req.body);
+    console.log(req.files);
 
-  car = await car
-    .save()
-    .then(() => {
-      res.send(car);
-    })
-    .catch((err) => {
-      console.log("error occured " + err);
+    const { files } = req;
+    for (let x = 0; x < files.otherImages.length; x++) {
+      otherImages.push(files.otherImages[x].originalname);
+    }
+    for (let x = 0; x < files.sendImages.length; x++) {
+      sendImages.push(files.sendImages[x].originalname);
+    }
+    const monthlyPayment =
+      (parseInt(req.body.price) - parseInt(req.body.downPayment)) /
+      parseInt(req.body.numberOfMonths);
+    console.log(monthlyPayment);
+    let car = new Car({
+      make: req.body.make,
+      name: req.body.name,
+      model: req.body.model,
+      displayImage: req.files.displayImage[0].originalname,
+      otherImages: otherImages,
+      fuel: req.body.fuel,
+      milesDriven: req.body.milesDriven,
+      body: req.body.body,
+      modelYear: req.body.modelYear,
+      exteriorColor: req.body.exteriorColor,
+      interiorColor: req.body.interiorColor,
+      engineType: req.body.engineType,
+      engineCapacity: req.body.engineCapacity,
+      transmission: req.body.transmission,
+      driveTrains: req.body.driveTrain,
+      doors: req.body.doors,
+      numberOfKeys: req.body.numberOfKeys,
+      vin: req.body.vin,
+      stock: req.body.stock,
+      vehicleId: req.body.vehicleId,
+      mpg: req.body.mpg,
+      extendedFeatures: req.body.extendedFeatures,
+      downPayment: req.body.downPayment,
+      monthlyPayment: monthlyPayment,
+      price: req.body.price,
+      shippingCharges: req.body.shippingCharges,
+      taxAndRegistrationCharges: req.body.taxAndRegistrationCharges,
+      dealerFees: req.body.dealerFees,
     });
-});
+
+    car = await car
+      .save()
+      .then(() => {
+        res.send(car);
+      })
+      .catch((err) => {
+        console.log("error occured " + err);
+      });
+  }
+);
 
 router.put("/:id", async (req, res) => {
   const car = await Car.updateOne(
